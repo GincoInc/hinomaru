91ee45d45e23562041e849346b6d0d6330619452
GuiltyMorishita <morilliantblue@gmail.com>
2023-05-03 17:46:24 +0900

    Add: store the generated key logic

--- pages/feature/password.tsx
+++ pages/feature/password.tsx
@@ -1,59 +1,58 @@
-import { ReactElement, useState, useEffect } from 'react'
-import FeaturePageLayout from '@/components/layout/featurePageLayout'
-import modalChild from '@styles/components/modal/login.module.scss'
-import { ModalContainer } from '@/components/modalContainer'
-import { ModalContentPassword } from '@/components/modal/password'
-import * as bip39 from 'bip39'
-import * as passworder from '@metamask/browser-passworder'
+import { ReactElement, useState, useEffect } from "react";
+import FeaturePageLayout from "@/components/layout/featurePageLayout";
+import modalChild from "@styles/components/modal/login.module.scss";
+import { ModalContainer } from "@/components/modalContainer";
+import { ModalContentPassword } from "@/components/modal/password";
+import { db, auth } from "@/utils/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import * as bip39 from "bip39";
+import * as passworder from "@metamask/browser-passworder";
 
 const FeaturePasswordModal = () => {
-  const [active, setActive] = useState(false)
-  const [isShowEncryptCompleteModal, setIsShowEncryptCompleteModal] =
-    useState(false)
-  const [password, setPassword] = useState<string>('')
-  const [encryptedPassword, setEncryptedPassword] = useState<Promise<string>>()
-  const [isModalActive, setIsModalActive] = useState(true)
+  const [active, setActive] = useState(false);
+  const [isShowEncryptCompleteModal, setIsShowEncryptCompleteModal] = useState(false);
+  const [password, setPassword] = useState<string>("");
+  const [encryptedPassword, setEncryptedPassword] = useState<Promise<string>>();
+  const [isModalActive, setIsModalActive] = useState(true);
 
-  const generateKey = async () => {
-    console.log('encrypting... ', password)
-    const mnemonic = bip39.generateMnemonic(256)
-    const seed = bip39.mnemonicToSeedSync(mnemonic)
-    return await passworder.encrypt(password, seed)
-  }
+  const generateMnemonicAndStore = async () => {
+    if (auth.currentUser) {
+      const userRef = doc(db, "users", auth.currentUser.uid);
+      const userSnap = await getDoc(userRef);
+      if (!userSnap.exists()) {
+        console.log("generate key");
+        const mnemonic = bip39.generateMnemonic(256);
+        const encrypted = await passworder.encrypt(password, mnemonic);
 
-  const encryptPassword = async () => {
-    const encryptedPassword = await generateKey()
-    console.log(encryptedPassword)
-    onClose()
-    // setEncryptedPassword(encryptedPassword)
-    //last
-    // setActive(true)
-    // setTimeout(() => {
-    //   setIsShowEncryptCompleteModal(true)
-    // }, 200)
-  }
+        console.log("store key");
+        await setDoc(doc(db, "users", auth.currentUser.uid), {
+          uid: auth.currentUser.uid,
+          encrypted: encrypted,
+        });
+      } else {
+        console.log("key already exists");
+        // TODO redirect to home
+      }
+    }
+  };
 
   const onClose = () => {
-    setIsModalActive(false)
-  }
+    setIsModalActive(false);
+  };
 
-  useEffect(() => {}, [])
+  useEffect(() => {}, []);
 
   return (
     <div className={`${modalChild.container}`}>
       <ModalContainer onClose={onClose} isModalActive={isModalActive}>
-        <ModalContentPassword
-          active={active}
-          onEncypt={encryptPassword}
-          setPassword={setPassword}
-        />
+        <ModalContentPassword active={active} onEncypt={generateMnemonicAndStore} setPassword={setPassword} />
       </ModalContainer>
     </div>
-  )
-}
+  );
+};
 
 FeaturePasswordModal.getLayout = (page: ReactElement) => {
-  return <FeaturePageLayout>{page}</FeaturePageLayout>
-}
+  return <FeaturePageLayout>{page}</FeaturePageLayout>;
+};
 
-export default FeaturePasswordModal
+export default FeaturePasswordModal;
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCc42aMrJbIHK_8wuWzkWrSYzjjigB3r10',
-  authDomain: 'hinomaru-hackathon.firebaseapp.com',
-  projectId: 'hinomaru-hackathon',
-  storageBucket: 'hinomaru-hackathon.appspot.com',
-  messagingSenderId: '41967019356',
-  appId: '1:41967019356:web:489f991906f2dc44e98d99',
-  measurementId: 'G-K7JN49DCG4',
+  apiKey: "AIzaSyCc42aMrJbIHK_8wuWzkWrSYzjjigB3r10",
+  authDomain: "hinomaru-hackathon.firebaseapp.com",
+  projectId: "hinomaru-hackathon",
+  storageBucket: "hinomaru-hackathon.appspot.com",
+  messagingSenderId: "41967019356",
+  appId: "1:41967019356:web:489f991906f2dc44e98d99",
+  measurementId: "G-K7JN49DCG4",
 };
 
 const app = initializeApp(config);
@@ -20,7 +20,7 @@ export { db, auth };
 
 export const getIdToken = async (): Promise<string> => {
   if (!auth.currentUser) {
-    return '';
+    return "";
   }
   return await auth.currentUser.getIdToken();
 };
